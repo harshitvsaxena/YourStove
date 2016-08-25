e7a3f8532dea7bf5d46f32e86870a8448d1ec82a
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-08-25 15:22:14 +0530

    Sentry added
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

--- app/index.js
+++ app/index.js
@@ -2,6 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Routes = require('./config/routes');
 
+/* --- Sentry Integration --- */
+var Raven = require('raven-js');
+var sentryKey = 'SENTRY KEY';
+var sentryApp = 'SENTRY APP ID';
+var sentryUrl = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp;
+Raven.config(sentryUrl).install();
+/* --- Sentry - End --- */
+
 ReactDOM.render(
     Routes,
     document.getElementById('app')
