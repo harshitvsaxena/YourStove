458aa37f319526ed90524eb4af2e299c5b21dee3
harshitvsaxena <harshitsaxena.com@gmail.com>
2016-09-05 12:36:15 +0530

    Some style fixes
    Signed-off-by: harshitvsaxena <harshitsaxena.com@gmail.com>

--- app/ui/Card.js
+++ app/ui/Card.js
@@ -9,8 +9,14 @@ function Card(props) {
     return (
         <div className="movie-card">
             <div className="box-container">
-                <a href="#"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/454262/MV5BMTQ0ODYzODc0OV5BMl5BanBnXkFtZTgwMDk3OTcyMDE%40._V1_SY1000_CR0%2C0%2C678%2C1000_AL_.jpg" alt="cover" className="cover" /></a>
+                <a href="#">
+                    <img 
+                        src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/454262/MV5BMTQ0ODYzODc0OV5BMl5BanBnXkFtZTgwMDk3OTcyMDE%40._V1_SY1000_CR0%2C0%2C678%2C1000_AL_.jpg"}
+                        alt="cover" 
+                        className="cover" />
+                </a>
                 <div className="back-cover">
+                    <img src={'#'} />
                     <div className="details">
                         <div className="title1">Breaking Bad <span>TV-14</span></div>
                         <div className="title2"> TV Series (2008–2013) </div>
